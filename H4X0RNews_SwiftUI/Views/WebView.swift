eb5d463d3449b1a200b5e3f0998df35744956f3b
aglegaspi <lex83nyc@gmail.com>
2020-09-03 17:45:42 -0400

    refactor WebView model

---
+++ H4X0RNews_SwiftUI/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  H4X0RNews_SwiftUI
+//
+//  Created by Alex 6.1 on 9/3/20.
+//  Copyright © 2020 aglegaspi. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<WebView>) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
