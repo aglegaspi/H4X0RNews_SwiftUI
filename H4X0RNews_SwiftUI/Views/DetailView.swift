c4dfdf1ddfd15c051ff200e27fb8170d9230626d
aglegaspi <lex83nyc@gmail.com>
2020-09-03 17:34:12 -0400

    set up DetailView, add NavigationLink and load DetailView upon selection from List View

---
+++ H4X0RNews_SwiftUI/Views/DetailView.swift
@@ -0,0 +1,26 @@
+//
+//  DetailView.swift
+//  H4X0RNews_SwiftUI
+//
+//  Created by Alex 6.1 on 9/3/20.
+//  Copyright © 2020 aglegaspi. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://google.com")
+    }
+}
+
+
